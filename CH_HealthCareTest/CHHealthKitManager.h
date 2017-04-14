cebf3019b2bd99a809316e02935a2dd77445d04f
chenhao <chenhao1@ngarihealth.com>
2017-04-14 17:23:41 +0800

    描述 以及leaks测试

--- CH_HealthCareTest/CHHealthKitManager.h
+++ CH_HealthCareTest/CHHealthKitManager.h
@@ -13,7 +13,7 @@
 
 
 /**
-  权限判断 以及对步数 
+  权限判断 以及对步数距离数据的写入 
  */
 @property (nonatomic, strong) HKHealthStore *healthStore;
 
@@ -30,29 +30,22 @@
  @param finish 检测是否获取到权限
  */
 -(void)getAuthorizedForHealthKit:(void(^)(BOOL isFinished))finish;
-
-
 /**
  获取步数
-
  @param completion 步数以及错误信息
  */
 - (void)getStepCount:(void(^)(double value, NSError *error))completion;
-
 /**
  获取距离
-
  @param completion 距离以及错误信息
  */
 - (void)getDistance:(void(^)(double value, NSError *error))completion;
-
 /**
  添加步数
 
  @param step 需要添加的步数
  */
 -(void)recordStep:(double)step callBack:(void(^)(BOOL success))completion;
-
 
 /**
  添加公里数
