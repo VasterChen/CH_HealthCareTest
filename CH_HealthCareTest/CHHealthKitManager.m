97da0ce5a5074638398567c98bd07c79615691de
chenhao <chenhao1@ngarihealth.com>
2017-04-14 15:46:02 +0800

    读取系统步数距离  以及修改

---
+++ CH_HealthCareTest/CHHealthKitManager.m
@@ -0,0 +1,178 @@
+//
+//  CHHealthKitManager.m
+//  CH_HealthCareTest
+//
+//  Created by 陈浩 on 2017/4/14.
+//  Copyright © 2017年 easyGroup. All rights reserved.
+//
+
+#import "CHHealthKitManager.h"
+#import <HealthKit/HealthKit.h>
+
+@implementation CHHealthKitManager
+
++(id)shareInstance{
+     static id manager ;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[self alloc]init];
+    });
+    return manager;
+}
+
+-(NSSet *)typesToWrite{
+    HKQuantityType *stepType = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+    HKQuantityType *distance = [HKObjectType quantityTypeForIdentifier: HKQuantityTypeIdentifierDistanceWalkingRunning];
+    return [NSSet setWithObjects:stepType,distance, nil];
+}
+
+
+//设置读写以下为设置的权限类型：
+- (NSSet *)typesToRead {
+    HKQuantityType *stepType = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+    HKQuantityType *distance = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning];
+    return [NSSet setWithObjects:stepType,distance, nil];
+}
+
+-(void)getAuthorizedForHealthKit:(void (^)(BOOL))finish{
+    //需要确定设备支持HealthKit
+    if (![HKHealthStore isHealthDataAvailable]) {
+        return;
+    }
+    if(self.healthStore == nil){
+        self.healthStore = [[HKHealthStore alloc] init];
+    }
+    NSSet * typesToShare = [self typesToWrite];
+    NSSet * typesToRead = [self typesToRead];
+    [self.healthStore requestAuthorizationToShareTypes:typesToShare readTypes:typesToRead completion:^(BOOL success, NSError * _Nullable error) {
+        if (success) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                finish(YES);
+            });
+        }else{
+            dispatch_async(dispatch_get_main_queue(), ^{
+                NSLog(@"Health has not authorized!");
+                finish(NO);
+            });
+        }
+    }];
+}
+
+//获取步数
+- (void)getStepCount:(void(^)(double value, NSError *error))completion
+{
+    HKQuantityType *stepType = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+    NSSortDescriptor *timeSortDescriptor = [[NSSortDescriptor alloc] initWithKey:HKSampleSortIdentifierEndDate ascending:NO];
+    // Since we are interested in retrieving the user's latest sample, we sort the samples in descending order, and set the limit to 1. We are not filtering the data, and so the predicate is set to nil.
+    HKSampleQuery *query = [[HKSampleQuery alloc] initWithSampleType:stepType predicate:[CHHealthKitManager predicateForSamplesToday] limit:HKObjectQueryNoLimit sortDescriptors:@[timeSortDescriptor] resultsHandler:^(HKSampleQuery *query, NSArray *results, NSError *error) {
+        if(error)
+        {
+            completion(0,error);
+        }
+        else
+        {
+            NSInteger totleSteps = 0;
+            for(HKQuantitySample *quantitySample in results)
+            {
+                HKQuantity *quantity = quantitySample.quantity;
+                HKUnit *heightUnit = [HKUnit countUnit];
+                double usersHeight = [quantity doubleValueForUnit:heightUnit];
+                totleSteps += usersHeight;
+            }
+            completion(totleSteps,error);
+        }
+    }];
+    
+    [self.healthStore executeQuery:query];
+}
+
+//获取公里数
+- (void)getDistance:(void(^)(double value, NSError *error))completion
+{
+    HKQuantityType *distanceType = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning];
+    NSSortDescriptor *timeSortDescriptor = [[NSSortDescriptor alloc] initWithKey:HKSampleSortIdentifierEndDate ascending:NO];
+    HKSampleQuery *query = [[HKSampleQuery alloc] initWithSampleType:distanceType predicate:[CHHealthKitManager predicateForSamplesToday] limit:HKObjectQueryNoLimit sortDescriptors:@[timeSortDescriptor] resultsHandler:^(HKSampleQuery * _Nonnull query, NSArray<__kindof HKSample *> * _Nullable results, NSError * _Nullable error) {
+        
+        if(error)
+        {
+            completion(0,error);
+        }
+        else
+        {
+            double totleSteps = 0;
+            for(HKQuantitySample *quantitySample in results)
+            {
+                HKQuantity *quantity = quantitySample.quantity;
+                HKUnit *distanceUnit = [HKUnit meterUnitWithMetricPrefix:HKMetricPrefixKilo];
+                double usersHeight = [quantity doubleValueForUnit:distanceUnit];
+                totleSteps += usersHeight;
+            }
+            completion(totleSteps,error);
+        }
+    }];
+    [self.healthStore executeQuery:query];
+}
+
+//添加步数
+-(void)recordStep:(double)step callBack:(void (^)(BOOL))completion{
+    //  categoryTypeForIdentifier:HKCategoryTypeIdentifierSleepAnalysis
+    HKQuantityType *stepType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+    if ([HKHealthStore isHealthDataAvailable] ) {
+        HKQuantity *stepQuantity = [HKQuantity quantityWithUnit:[HKUnit countUnit] doubleValue:step];
+        HKQuantitySample *stepSample = [HKQuantitySample quantitySampleWithType:stepType quantity:stepQuantity startDate:[NSDate date] endDate:[NSDate date]];
+        [self.healthStore saveObject:stepSample withCompletion:^(BOOL success, NSError *error) {
+            if (success) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    completion(YES);
+                });
+            }else{
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    completion(NO);
+                });
+            }
+        }];
+    }
+}
+
+//添加公里数
+-(void)recordKilo:(double)kilo callBack:(void (^)(BOOL))completion{
+    //  categoryTypeForIdentifier:HKCategoryTypeIdentifierSleepAnalysis
+    HKQuantityType *stepType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning];
+    if ([HKHealthStore isHealthDataAvailable] ) {
+        HKQuantity *stepQuantity = [HKQuantity quantityWithUnit:[HKUnit meterUnitWithMetricPrefix:HKMetricPrefixKilo] doubleValue:kilo];
+        HKQuantitySample *stepSample = [HKQuantitySample quantitySampleWithType:stepType quantity:stepQuantity startDate:[NSDate date] endDate:[NSDate date]];
+        [self.healthStore saveObject:stepSample withCompletion:^(BOOL success, NSError *error) {
+            if (success) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    completion(YES);
+                });
+            }else{
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    completion(NO);
+                });
+            }
+        }];
+    }
+}
+
+
+/*!
+ *  @brief  当天时间段
+ *
+ *  @return 时间段
+ */
++ (NSPredicate *)predicateForSamplesToday {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDate *now = [NSDate date];
+    NSDateComponents *components = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:now];
+    [components setHour:0];
+    [components setMinute:0];
+    [components setSecond: 0];
+    
+    NSDate *startDate = [calendar dateFromComponents:components];
+    NSDate *endDate = [calendar dateByAddingUnit:NSCalendarUnitDay value:1 toDate:startDate options:0];
+    NSPredicate *predicate = [HKQuery predicateForSamplesWithStartDate:startDate endDate:endDate options:HKQueryOptionNone];
+    return predicate;
+}
+
+@end
--- CH_HealthCareTest/ViewController.m
+++ CH_HealthCareTest/ViewController.m
@@ -7,19 +7,70 @@
 //
 
 #import "ViewController.h"
+#import "CHHealthKitManager.h"
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *addStepTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *systemStepLbl;
+
+@property (weak, nonatomic) IBOutlet UITextField *addKiloTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *systemKiloLbl;
+
+
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    CHHealthKitManager *_manager;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+     _manager = [CHHealthKitManager shareInstance];
+    [_manager getAuthorizedForHealthKit:^(BOOL isFinished) {
+         //YES 说明获取到了权限
+        if (isFinished) {
+           
+        }
+    }];
+    [_manager getStepCount:^(double value, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _systemStepLbl.text=[NSString stringWithFormat:@"%.f步", value];
+        });
+    }];
+    [_manager getDistance:^(double value, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _systemKiloLbl.text=[NSString stringWithFormat:@"%.f公里", value];
+        });
+    }];
+
 }
 
+- (IBAction)addStepCount:(UIButton *)sender {
+    __block typeof(self) weakSelf = self;
+    [_manager recordStep:[_addStepTextField.text doubleValue] callBack:^(BOOL success) {
+        if (success) {
+           __block typeof(weakSelf) strongSelf = weakSelf;
+            double totalCount = [_systemStepLbl.text doubleValue] + [_addStepTextField.text doubleValue];
+            strongSelf -> _systemStepLbl.text = [NSString stringWithFormat:@"%.f步",totalCount];
+        }
+    }];
+}
 
+- (IBAction)addKiloCount:(UIButton *)sender {
+    __block typeof(self) weakSelf = self;
+    [_manager recordKilo:[_addKiloTextField.text doubleValue] callBack:^(BOOL success) {
+        if (success) {
+            __block typeof(weakSelf) strongSelf = weakSelf;
+            double totalCount = [_systemKiloLbl.text doubleValue] + [_addKiloTextField.text doubleValue];
+            strongSelf -> _systemKiloLbl.text = [NSString stringWithFormat:@"%.f公里",totalCount];
+        }
+    }];
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
